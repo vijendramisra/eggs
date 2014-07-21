16dc03e60c7ad371f5c22028ff4378543183fbde
Vijendra Mishra <iamonetoughguy@gmail.com>
2014-07-21 20:11:30 +0530

    major changes

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require("express");
+//var logfmt = require("logfmt");
+var app = express();
 
+//app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res){
-	res.sendfile('index.html');
-});
-
-io.on('connection', function(socket){
-	console.log("a user connected");
-
-	socket.on('disconnect', function(){
-    	console.log('user disconnected');
-  	});
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
-
-http.listen(port_number, function(){
-	console.log('listening on *:3000')
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
