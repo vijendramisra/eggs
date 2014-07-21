23d4222b75f296e4a4adc3ceb0f15bb858f3b02d
Vijendra Mishra <iamonetoughguy@gmail.com>
2014-07-21 16:44:51 +0530

    Port number

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port_number = server.listen(process.env.PORT || 3000);
+
 app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
@@ -14,6 +16,6 @@ io.on('connection', function(socket){
   	});
 });
 
-http.listen(3000, function(){
+http.listen(port_number, function(){
 	console.log('listening on *:3000')
 });
