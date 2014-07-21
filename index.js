7b5de5bb5672543377a4f302a5cd2ca8b1376a39
Vijendra Mishra <iamonetoughguy@gmail.com>
2014-07-21 20:30:47 +0530

    Changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 
 var port = Number(process.env.PORT || 5000);
 
-app.listen(port_number, function(){
+http.listen(port, function(){
 	console.log('listening on *:3000')
 });
 
