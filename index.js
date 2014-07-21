8c4b38577374cd3abc7735105b6d9ea0d97f1971
Vijendra Mishra <iamonetoughguy@gmail.com>
2014-07-21 19:52:06 +0530

    changes

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ io.on('connection', function(socket){
 
 var port = Number(process.env.PORT || 5000);
 
-http.listen(port_number, function(){
+app.listen(port_number, function(){
 	console.log('listening on *:3000')
 });
